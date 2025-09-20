eddb840642e28de8cea42a73b1d76f2807b86b16
Dmitry Konishchev <konishchev@gmail.com>
2025-09-20 16:24:26 +0300

    Introduce test options

--- pkg/test/feed.go
+++ pkg/test/feed.go
@@ -11,8 +11,13 @@ import (
 	"github.com/KonishchevDmitry/feedsd/pkg/test/testutil"
 )
 
-func Feed(t *testing.T, generator feed.Feed, mayBeEmpty bool) {
+func Feed(t *testing.T, generator feed.Feed, opts ...FeedOption) {
 	t.Parallel()
+
+	var options options
+	for _, opt := range opts {
+		opt(&options)
+	}
 
 	ctx := testutil.Context(t)
 	ctx = fetch.WithContext(ctx, prometheus.NewHistogram(prometheus.HistogramOpts{}))
@@ -20,11 +25,23 @@ func Feed(t *testing.T, generator feed.Feed, mayBeEmpty bool) {
 	feed, err := generator.Get(ctx)
 	require.NoError(t, err)
 
-	if !mayBeEmpty {
+	if !options.mayBeEmpty {
 		require.NotEmpty(t, feed.Items)
 	}
 
 	for _, item := range feed.Items {
 		require.NotEmpty(t, item.Description)
+	}
+}
+
+type options struct {
+	mayBeEmpty bool
+}
+
+type FeedOption func(o *options)
+
+func MayBeEmpty() FeedOption {
+	return func(o *options) {
+		o.mayBeEmpty = true
 	}
 }
