cfa6c6a2247afa7987de22e036ddc940aae51d03
Dmitry Konishchev <konishchev@gmail.com>
2026-01-27 19:14:00 +0300

    Support parametrized scrapers in registry

--- internal/scraper/registry.go
+++ internal/scraper/registry.go
@@ -9,32 +9,52 @@ import (
 )
 
 type Registry struct {
-	// FIXME(konishchev): Add simple scrapers
-	scrapers map[string]*BackgroundScraper
+	scrapers           map[string]struct{}
+	backgroundScrapers []*BackgroundScraper
 	metrics
 }
 
 func NewRegistry() *Registry {
 	return &Registry{
-		scrapers: make(map[string]*BackgroundScraper),
+		scrapers: make(map[string]struct{}),
 		metrics:  makeMetrics(),
 	}
 }
 
 func (r *Registry) Add(feed feed.Feed) (*BackgroundScraper, error) {
 	name := feed.Name()
-	if _, ok := r.scrapers[name]; ok {
-		return nil, fmt.Errorf("%q feed is already registered", name)
+
+	if err := r.add(name); err != nil {
+		return nil, err
 	}
 
-	scraper := newBackgroundScraper(feed, &r.metrics)
-	r.scrapers[name] = scraper
+	scraper := newBackgroundScraper(feed, r.metrics.baseObservers(name), r.metrics.backgroundObservers(name))
+	r.backgroundScrapers = append(r.backgroundScrapers, scraper)
 
 	return scraper, nil
 }
 
+func AddParametrized[P feed.Params](r *Registry, feed feed.ParametrizedFeed[P]) (*SimpleParametrizedScraper[P], error) {
+	name := feed.Name()
+
+	if err := r.add(name); err != nil {
+		return nil, err
+	}
+
+	scraper := newSimpleParametrizedScraper(feed, r.metrics.baseObservers(name))
+	return scraper, nil
+}
+
+func (r *Registry) add(name string) error {
+	if _, ok := r.scrapers[name]; ok {
+		return fmt.Errorf("%q feed is already registered", name)
+	}
+	r.scrapers[name] = struct{}{}
+	return nil
+}
+
 func (r *Registry) Start(ctx context.Context, develMode bool) {
-	for _, scraper := range r.scrapers {
+	for _, scraper := range r.backgroundScrapers {
 		scraper.start(ctx, develMode)
 	}
 }
@@ -43,7 +63,7 @@ func (r *Registry) Stop(ctx context.Context) {
 	var waitGroup sync.WaitGroup
 	defer waitGroup.Wait()
 
-	for _, scraper := range r.scrapers {
+	for _, scraper := range r.backgroundScrapers {
 		waitGroup.Go(func() {
 			scraper.stop(ctx)
 		})
